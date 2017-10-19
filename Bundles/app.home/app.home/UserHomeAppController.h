ee4b2b9832d28382d810531117e3b769d9b325d9
wanyudong <wan940317@gmail.com>
2017-10-19 09:17:40 +0800

    新增pch

--- Bundles/app.home/app.home/UserHomeAppController.h
+++ Bundles/app.home/app.home/UserHomeAppController.h
@@ -6,7 +6,6 @@
 //  Copyright © 2017年 Eazytec. All rights reserved.
 //
 
-#import "com_eazytec_bpm_lib_common/lib.common.h"
 
 @interface UserHomeAppController : EAViewController<UICollectionViewDataSource,UICollectionViewDelegateFlowLayout,UICollectionViewDelegate>
 
