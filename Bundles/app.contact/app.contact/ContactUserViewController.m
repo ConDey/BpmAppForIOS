0d64fcf6e81b02a4fc697975bb68e7d5af12a98a
dong <2569263976@qq.com>
2017-10-23 09:38:32 +0800

    用户打电话

--- Bundles/app.contact/app.contact/ContactUserViewController.m
+++ Bundles/app.contact/app.contact/ContactUserViewController.m
@@ -21,13 +21,87 @@
     [self setTitleOfNav:self.user.fullName];
     //没图
     UIImageView *picView=[[UIImageView alloc]init];
-    picView.backgroundColor=[UIColor redColor];
+    picView.image=[UIImage imageNamed:@"app_banner.png" inBundle:self.bundle compatibleWithTraitCollection:nil];
     [self.view addSubview:picView];
     [picView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.right.left.mas_equalTo(0);
         make.top.mas_equalTo(0);
         make.height.mas_equalTo(150);
     }];
+    //信息电话按钮
+    UIView *callAndMsg=[[UIView alloc]init];
+    [self.view addSubview:callAndMsg];
+    [callAndMsg mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(picView.mas_bottom).mas_equalTo(5);
+        make.left.right.mas_equalTo(0);
+        make.height.mas_equalTo(90);
+    }];
+    UIView *msg=[[UIView alloc]init];
+     UIView *call=[[UIView alloc]init];
+    [callAndMsg addSubview:msg];
+    [msg mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.size.mas_equalTo(CGSizeMake(60, 60));
+        make.left.mas_equalTo(60);
+        make.top.mas_equalTo(10);
+    }];
+    [callAndMsg addSubview:call];
+    [call mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.size.mas_equalTo(CGSizeMake(60, 60));
+        make.right.mas_equalTo(-60);
+        make.top.mas_equalTo(10);
+    }];
+    UILabel *la1=[[UILabel alloc]init];
+    UILabel *la2=[[UILabel alloc]init];
+    [msg addSubview:la1];
+    [call addSubview:la2];
+    UIImageView *ig1=[[UIImageView alloc]init];
+    UIImageView *ig2=[[UIImageView alloc]init];
+    [msg addSubview:ig1];
+    [call addSubview:ig2];
+    [ig1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.size.mas_equalTo(CGSizeMake(40, 40));
+        make.left.mas_equalTo(0);
+        make.top.mas_equalTo(0);
+    }];
+    [ig2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.size.mas_equalTo(CGSizeMake(40, 40));
+        make.right.mas_equalTo(0);
+        make.top.mas_equalTo(0);
+    }];
+    [la1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.mas_equalTo(ig1.mas_centerX);
+        make.top.mas_equalTo(ig1.mas_bottom).mas_equalTo(0);
+        make.height.mas_equalTo(20);
+        make.width.mas_equalTo(60);
+    }];
+    [la2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.mas_equalTo(ig2.mas_centerX);
+        make.top.mas_equalTo(ig2.mas_bottom).mas_equalTo(0);
+        make.height.mas_equalTo(20);
+        make.width.mas_equalTo(60);
+    }];
+    la1.text=@"发信息";
+    la2.text=@"打电话";
+    la1.textAlignment=NSTextAlignmentCenter;
+    la2.textAlignment=NSTextAlignmentCenter;
+    ig1.image=[UIImage imageNamed:@"ic_contact_way_msg.png" inBundle:self.bundle compatibleWithTraitCollection:nil];
+    ig2.image=[UIImage imageNamed:@"ic_contact_way_tel.png" inBundle:self.bundle compatibleWithTraitCollection:nil];
+    ig1.contentMode=UIViewContentModeScaleToFill;
+    ig2.contentMode=UIViewContentModeScaleToFill;
+    //点击发信息
+    UITapGestureRecognizer *tapMeg=[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tapMeg:)];
+    [msg addGestureRecognizer:tapMeg];
+  
+    //点击打电话
+    UITapGestureRecognizer *tapTel=[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tapTel:)];
+    [call addGestureRecognizer:tapTel];
+
+    
+    
+    
+    
+    
+    
     self.grouptableview.dataSource  = self;
     self.grouptableview.delegate=self;
     [self.grouptableview setSeparatorStyle:UITableViewCellSeparatorStyleSingleLine];
@@ -35,7 +109,8 @@
     
     [self.view addSubview:self.grouptableview];
     [self.grouptableview mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.edges.mas_equalTo(UIEdgeInsetsMake(150, 0, 0, 0));
+        make.left.right.bottom.mas_equalTo(0);
+        make.top.mas_equalTo(callAndMsg.mas_bottom).mas_equalTo(2);
     }];
     
 }
@@ -71,14 +146,13 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 3;
+    return 2;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     if(section==0){
-        return 1;
-    }else if (section==1){
         return 3;
+    
     }else{
         return 2;
     }
@@ -86,11 +160,7 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     ContractUserTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ContractUserTableViewCell" forIndexPath:indexPath];
-    if(indexPath.section==0){
-        cell.leftImageView.image =  [[UIImage alloc]init];
-        cell.titleLabel.text = @"";
-        cell.detailLabel.text = @"";
-    }else if (indexPath.section==1){
+    if (indexPath.section==0){
         if (indexPath.row == 0) {
             cell.leftImageView.image =  [UIImage imageNamed:@"ic_contact_user" inBundle:self.bundle compatibleWithTraitCollection:nil];
             cell.titleLabel.text = @"人员姓名";
@@ -108,7 +178,7 @@
             cell.titleLabel.text = @"工作职位";
             cell.detailLabel.text = self.user.position;
         }
-    }else if (indexPath.section==2){
+    }else {
         if(indexPath.row == 0) {
             cell.leftImageView.image =  [UIImage imageNamed:@"ic_contact_tel" inBundle:self.bundle compatibleWithTraitCollection:nil];
             cell.titleLabel.text = @"联系方式";
@@ -136,66 +206,18 @@
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    if(indexPath.section==0){
-        return 0.01;
-    }else{
+   
         return 70;
-    }
+    
 }
 
 -(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
-    if(section==0){
-        return 90;
-    }else{
+   
         return 60;
-    }
+    
 }
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
-    if(section==0){
-        UIView *headerView=[[UIView alloc]initWithFrame:CGRectMake(0, 0, self.grouptableview.bounds.size.width, 70)];
-        headerView.backgroundColor=LIGHT_GRAY_COLOR;
-        UILabel *la1=[[UILabel alloc]init];
-        UILabel *la2=[[UILabel alloc]init];
-        [headerView addSubview:la1];
-        [headerView addSubview:la2];
-        UIImageView *ig1=[[UIImageView alloc]init];
-        UIImageView *ig2=[[UIImageView alloc]init];
-        [headerView addSubview:ig1];
-        [headerView addSubview:ig2];
-        [ig1 mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.size.mas_equalTo(CGSizeMake(40, 40));
-            make.left.mas_equalTo(60);
-            make.top.mas_equalTo(10);
-        }];
-        [ig2 mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.size.mas_equalTo(CGSizeMake(40, 40));
-            make.right.mas_equalTo(-60);
-            make.top.mas_equalTo(10);
-        }];
-        [la1 mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.centerX.mas_equalTo(ig1.mas_centerX);
-            make.top.mas_equalTo(ig1.mas_bottom).mas_equalTo(0);
-            make.height.mas_equalTo(20);
-            make.width.mas_equalTo(60);
-        }];
-        [la2 mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.centerX.mas_equalTo(ig2.mas_centerX);
-            make.top.mas_equalTo(ig2.mas_bottom).mas_equalTo(0);
-            make.height.mas_equalTo(20);
-            make.width.mas_equalTo(60);
-        }];
-        la1.text=@"发信息";
-        la2.text=@"打电话";
-        la1.textAlignment=NSTextAlignmentCenter;
-        la2.textAlignment=NSTextAlignmentCenter;
-        ig1.image=[UIImage imageNamed:@"ic_contact_user.png" inBundle:self.bundle compatibleWithTraitCollection:nil];
-        ig2.image=[UIImage imageNamed:@"ic_contact_tel.png" inBundle:self.bundle compatibleWithTraitCollection:nil];
-        ig1.contentMode=UIViewContentModeScaleToFill;
-        ig2.contentMode=UIViewContentModeScaleToFill;
-        return headerView;
-        
-    }
-    else if(section == 1) {
+    if(section==0) {
         UIView *headerView=[[UIView alloc]initWithFrame:CGRectMake(0, 0, self.grouptableview.bounds.size.width, 60)];
         UILabel *Msg1=[[UILabel alloc]init];
         [headerView addSubview:Msg1];
@@ -225,7 +247,23 @@
     }
 }
 
+//打电话 发信息
+-(void)tapMeg:(UITapGestureRecognizer *)sender{
+    
+}
 
+-(void)tapTel:(UITapGestureRecognizer *)sender{
+    UIAlertController *call=[UIAlertController alertControllerWithTitle:@"是否要拨打电话？" message:nil preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *cancel=[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil];
+    UIAlertAction *sure=[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        NSLog(@"boda");
+          [[UIApplication sharedApplication] openURL:[NSURL URLWithString:[@"tel://" stringByAppendingString:self.user.mobile]]];
+    }];
+    [call addAction:cancel];
+    [call addAction:sure];
+    [self presentViewController:call animated:YES completion:nil];
+  
+}
 #pragma mark - Table view setting
 
 // 自定义TableViewCell分割线, 清除前面15PX的空白
